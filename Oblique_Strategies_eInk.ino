b4fb3936b66234372a2035c0bb68e9bf580915e7
acbakhit <amandwato@gmail.com>
2019-05-29 07:42:06 -0700

    121-130

--- Oblique_Strategies_eInk.ino
+++ Oblique_Strategies_eInk.ino
@@ -1,4 +1,4 @@
-#include <GxEPD.h>
+t#include <GxEPD.h>
 #include <GxGDEH029A1/GxGDEH029A1.cpp>      // 2.9" b/w
 #include <GxIO/GxIO_SPI/GxIO_SPI.cpp>
 #include <GxIO/GxIO.cpp>
@@ -21,7 +21,7 @@ void setup() {
 void loop() {
 
   while (1){
-  int n = random(0, 118);
+  int n = random(0, 128);
   display.drawExampleBitmap(IMAGE[n], 0, 0, GxEPD_WIDTH,GxEPD_HEIGHT, GxEPD_BLACK);
   display.update();
   esp_sleep_enable_timer_wakeup(86400000000);
